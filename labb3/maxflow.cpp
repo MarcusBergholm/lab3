451abf16405bb57092264c3f0d998f4867f4f1e2
MarcusBergholm <marcusbergholm@hotmail.com>
2015-11-01 18:32:14 +0100

    Sooooon!

--- labb3/maxflow.cpp
+++ labb3/maxflow.cpp
@@ -1,75 +1,137 @@
 #include <iostream>
 #include <sstream>
 #include <vector>
-#include <list>
 #include <queue>
 
 using namespace std;
 
-int v, first, last, e;
-
-int x, y, z;
-
 struct Edge {
-	int sink;
-	int capacity;
-	int rCapacity;
-	int flow;
-	bool visited;
-	Edge() : 
-		sink(0), 
-		capacity(0), 
-		rCapacity(0), 
-		flow(0),
-		visited(false){}
-	Edge(int x1, int x2) : 
-		sink(x1), 
-		capacity(x2), 
-		rCapacity(x2), 
-		flow(0),
-		visited(false){}
+    int origin;
+    int sink;
+    int capacity;
+    int flow;
+    Edge * rev;
+    Edge(int x, int x1, int x2) :
+        origin(x),
+        sink(x1),
+        capacity(x2),
+        flow(0)
+    {}
 };
 
+vector< vector<Edge*> > vertices;
+int v, first, last, e;
+int x, y, z;
 
-vector< vector<Edge> > createGraph() {
-	cin >> v >> first >> last >> e;
-	// vector< list<Edge> > vertices(v + 1);
-	vector< vector<Edge> > vertices(v,vector<Edge>());
+void createGraph() {
+    vector< vector<Edge*> > hotshot(v+1,vector<Edge*>());
 
-	for (int i = 0; i < e; ++i) {
-		cin >> x >> y >> z;
-		Edge e1(x,z);
-		Edge e2(y,z);
-		vertices[x-1].push_back(e2);
-		vertices[y-1].push_back(e1);
-	}
-	return vertices;
+    for(int i = 0; i < e; ++i) {
+        cin >> x >> y >> z;
+        Edge *e1 = new Edge(x,y,z);
+        Edge *e2 = new Edge(y,x,0);
+        e1->rev = e2;
+        e2->rev = e1;
+        hotshot[x].push_back(e1);
+        hotshot[y].push_back(e2);
+    }
+    vertices = hotshot;
 }
 
-Edge bfs(vector< vector< Edge > > vertices) {
-	queue<Edge> queue;
-	Edge curry;
-	for (int i = 0; i < vertices[0].size(); ++i) {
-		queue.push(vertices[0][i]);
-	}
-	while (!queue.empty()) {
-		curry = queue.front();
-		cout << curry.sink << endl;
-		curry.visited = true;
-		// for (int i = 0; i < vertices[curry.sink].size(); ++i) {
-		// 	if(!(vertices[curry.sink][i].visited)) {
-		// 		queue.push(vertices[curry.sink][i]);
-		// 	}
-		// }
-		queue.pop();
-	}
+vector<Edge*> genPath() {
+    cout << "FIND PATH" << endl;
+    queue<int> queue;
+    int current;
+    queue.push(first);
+    vector<Edge*> path(v+1, nullptr);
 
-	return curry;
+    while (!queue.empty()) {
+        current = queue.front();
+        queue.pop();
+
+        if(current == 0) {}
+        else {
+            for(int i = 0; i < vertices[current].size(); ++i) {
+                Edge * e = vertices[current][i];
+                if(path[e->sink] == nullptr) {
+                    if(e->capacity > e->flow && e->sink != first) {
+                        path[e->sink] = vertices[current][i];
+                        cout << e->origin << " ";
+                        cout << e->sink << endl;
+                        queue.push(e->sink);
+                    }
+                }
+            }
+        }
+    }
+
+    cout << "DONE PATH" << endl;
+    return path;
+}
+
+void findMaxFlow() {
+    while(true) {
+        auto path = genPath();
+        int pos = last;
+        int rCap = (path[last]->capacity) - (path[last]->flow);
+        vector<Edge*> origins;
+        if(path[last] == nullptr) {
+            break;
+        }
+
+        origins.push_back(path[last]);
+        cout << "FIND RCAP" << endl;
+        while(pos > 2) {
+            cout << pos << endl;
+            pos = path[pos]->origin;
+            origins.push_back(path[pos]);
+            if(rCap > (path[pos]->capacity) - (path[pos]->flow)) {
+                rCap = (path[pos]->capacity) - (path[pos]->flow);
+            }
+        }
+        cout << "DONE RCAP" << endl;
+
+        for(Edge * e : origins) {
+            e->flow = (e->flow) + rCap;
+            e->rev->flow = (e->rev->flow) - rCap;
+        }
+    }
+
+    // cout << endl;
+    // for(Edge * e: path) {
+    //     if(e == nullptr) {
+
+    //     } else {
+    //         cout << e->origin << " " << e->sink << endl;
+    //     }
+    // }
 }
 
 int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(0);
-	bfs(createGraph());
-	return 0;
+    // haxxzor out in
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    cin >> v >> first >> last >> e;
+
+    createGraph();
+    findMaxFlow();
+
+    return 0;
 }
+
+
+// 9
+// 1 9
+// 11
+// 1 2 1
+// 1 5 7
+// 2 3 1
+// 3 4 1
+// 3 8 2
+// 3 7 1
+// 3 6 4
+// 4 5 1
+// 6 9 1
+// 7 9 2
+// 8 7 1
